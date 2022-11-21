513bf5f31d5872866a253ec0bbb033112bf7a678
Oluwapelumi Ayeni <noluwapelumi4@gmail.com>
2022-11-21 22:31:01 +0000

    Added HitEffect

---
+++ Assets/Scripts/Universal/FlashEffect.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AllIn1SpriteShader;
+
+public class FlashEffect : MonoBehaviour
+{
+    [SerializeField] private float duration;
+
+    private Material mat;
+    
+    void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    public void Flash()
+    {
+        StartCoroutine(FlashRoutine());
+    }
+    
+    IEnumerator FlashRoutine()
+    {
+        float dissolveValue = 1f;
+        while (dissolveValue > 0f)
+        {
+            dissolveValue -= Time.deltaTime / duration;
+            dissolveValue = Mathf.Clamp01(dissolveValue);
+            mat.SetFloat("_HitEffectBlend", dissolveValue);
+            yield return null;
+        }
+    }
+
+    public void ToggleFlash(bool toggle)
+    {
+        mat.EnableKeyword(toggle ? "HITEFFECT_ON" : "HITEFFECT_OFF");
+    }
+}
